928f73cc0deb9151c5bd7faad6baebc88847c6e0
charudatta10 <100494c@gmail.com>
2025-06-01 14:54:27 +0530

    feat: Add initial project structure with essential files and templates

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# © 2025 Charudatta Korde. Some Rights Reserved. Attribution Required. Non-Commercial Use & Share-Alike.
+# https://raw.githubusercontent.com/charudatta10/task-runner-SDLC/refs/heads/main/src/templates/LICENSE
